d7512256c0259fa75e1a038cba2a038bda8da4de
Jaime Cernuda <jcernudagarcia@hawk.iit.edu>
2026-08-05 23:51:18 -0500

    fix(jarvis): ship JARVIS 1.8.0 in 2.6.6 (#348)
    * fix(jarvis): ship JARVIS 1.8.0 in 2.6.6
    
    * fix(deps): update cryptography to 50.0.0
    
    * fix(deps): update aiohttp to 3.14.3

--- clio-kit-website/src/data/mcpData.js
+++ clio-kit-website/src/data/mcpData.js
@@ -256,7 +256,7 @@ export const mcpData = {
       "jarvis_get_execution"
     ],
     "stats": {
-      "version": "3.6.1",
+      "version": "3.6.2",
       "updated": "2026-07-18"
     },
     "platforms": [
